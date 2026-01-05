cd509420d0fddb07aa05281c477a40e9053eb4c8
xHugo21 <hugo2121h@gmail.com>
2026-01-05 19:19:28 +0100

    feat: use metric-ff from ogamus folder instead of metric-ff from macos specific submodule. remove submodule

--- README.md
+++ README.md
@@ -82,4 +82,3 @@ gsettings set org.gnome.mutter check-alive-timeout 60000
 - iTHOR documentation: https://ai2thor.allenai.org/ithor/documentation/
 - LAMANNA, Leonardo, et al. Online grounding of symbolic planning domains in unknown environments. En Proceedings of the International Conference on Principles of Knowledge Representation and Reasoning. 2022. p. 511-521. [PDF](https://arxiv.org/pdf/2112.10007.pdf). [GitHub](https://github.com/LamannaLeonardo/OGAMUS)
 - [Metric-FF](https://fai.cs.uni-saarland.de/hoffmann/metric-ff.html) planner
-- [tranchis](https://github.com/tranchis/metric-ff-macos) macos compilable version of [Metric-FF](https://fai.cs.uni-saarland.de/hoffmann/metric-ff.html)
--- planner.py
+++ planner.py
@@ -2,14 +2,23 @@
 import os
 
 
-class Planner():
-    '''Class that contains the methods to call the planner and manage generated plans'''
+class Planner:
+    """Class that contains the methods to call the planner and manage generated plans"""
 
-    def __init__(self, problem_path, output_path, problem, search_algorithm, heuristic, print=False, ogamus=False):
+    def __init__(
+        self,
+        problem_path,
+        output_path,
+        problem,
+        search_algorithm,
+        heuristic,
+        print=False,
+        ogamus=False,
+    ):
         # Selects domain depending on the method selected
-        self.domain_path = './pddl/domain_input.pddl'
+        self.domain_path = "./pddl/domain_input.pddl"
         if not ogamus:
-            self.domain_path = f'./pddl/domain_{problem}.pddl'
+            self.domain_path = f"./pddl/domain_{problem}.pddl"
         self.problem_path = problem_path
         self.output_path = output_path
 
@@ -24,20 +33,21 @@ class Planner():
             self.print_plan()
 
     def run_plan_cbp(self):
-        '''Method that executes cbp_roller using argument paths'''
+        """Method that executes cbp_roller using argument paths"""
         try:
-            os.system(f'./planner/metric-ff/ff -o {self.domain_path} -f {
-                      self.problem_path} > {self.output_path}')
+            os.system(
+                f"./OGAMUS/Plan/PDDL/Planners/FF/ff -o {self.domain_path} -f {self.problem_path} > {self.output_path}"
+            )
         except FileNotFoundError:
             raise Exception("Error executing planner: File not found\n")
 
     def print_plan(self):
-        '''Method that prints plan via CLI'''
-        with open(self.output_path, 'r') as f:
+        """Method that prints plan via CLI"""
+        with open(self.output_path, "r") as f:
             print(f.read())
 
     def get_plan(self):
-        '''Method which saves and returns the plan inside a variable'''
-        with open(self.output_path, 'r') as f:
+        """Method which saves and returns the plan inside a variable"""
+        with open(self.output_path, "r") as f:
             raw_plan = f.read()
         return raw_plan
